ad27a556ef5327fdcd48ad699033da4bcee8a5e5
SandeepKushwaha <sandeepkush327@gmail.com>
2023-09-07 14:31:12 +0530

    component CardItem created.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <CardItem />
+      <div className='Container'>
+        <CardItem />
+      </div>
     </div>
   );
 }
--- src/CardItem.js
+++ src/CardItem.js
@@ -3,18 +3,53 @@ import './CardItem.css';
 
 // class based component
 class CardItem extends React.Component {
+
+    constructor() { 
+        super();
+        this.state = {
+            title: 'Smart Phone',
+            price: 12999,
+            quantity: 1,
+            image: '',
+        };
+        // this.increaseQuantity = this.increaseQuantity.bind(this);
+    }
+
+    increaseQuantity = () => {
+        console.log('increase: called!');
+        console.log('This Object:', this);
+        console.log('This Object State:', this.state);
+    }
+
     render() {
+        const { title, price, quantity, image } = this.state;
         return (
             <div className="card-item">
                 <div className="left-block">
-                    <img style={styles.image} src="" alt="" />
+                    <img style={styles.image} src={ image } alt="" />
                 </div>
                 <div className="right-block">
-                    <div style={styles.title}>Phone</div>
-                    <div style={styles.description}>Rs: 999</div>
-                    <div style={ {color: "#777"} }>Quntity: 1</div>
+                    <div style={styles.title}>{ title }</div>
+                    <div style={styles.description}>Rs: { price }</div>
+                    <div style={ {color: "#777"} }>Quantity: { quantity }</div>
                     <div className="card-item-actions">
                         {/* Buttons */}
+                        <img
+                            alt="increase"
+                            className="action-icons"
+                            src="https://cdn-icons-png.flaticon.com/512/3161/3161837.png"
+                            onClick={ this.increaseQuantity }
+                        />
+                        <img
+                            alt="decrease"
+                            className="action-icons"
+                            src="https://cdn-icons-png.flaticon.com/512/2040/2040522.png"
+                        />
+                        <img
+                            alt="delete"
+                            className="action-icons"
+                            src="https://cdn-icons-png.flaticon.com/512/3405/3405244.png"
+                        />
                     </div>
                 </div>
             </div>
